98a48469256d96e0642c329bed304f200612a473
shwetasrikanth <60562241+shwetasrikanth@users.noreply.github.com>
2022-12-05 13:17:09 -0500

    Edited codes for finding top 10

--- lightest_dogs.R
+++ lightest_dogs.R
@@ -6,7 +6,7 @@ d2<-d1[order(-d1$avg_w),]
 
 
 
-ggplot(d2[125:134,],aes(x=reorder(Breed, avg_w), y=avg_w)) + geom_bar(stat="identity", fill="blue") + 
+ggplot(d2[125:134,],aes(x=reorder(Breed, avg_w), y=avg_w)) + geom_bar(stat="identity", fill="purple") + 
   ggtitle("Top 10 lightest dogs") + labs(x= "Breed", y="Average Weight of Dogs") + theme(text = element_text(size=10),
                                                                                          axis.text.x = element_text(angle=90, hjust=1))
 
--- not_smart_dogs.R
+++ not_smart_dogs.R
@@ -5,9 +5,9 @@ d1<- read.csv("derived_data/derived_data_reps.csv")
 d2<-d1[order(-d1$avg_h),]
 
 
-ggplot(d2[1:10,],aes(x=reorder(Breed, avg_reps), y=avg_reps)) + geom_bar(stat="identity", fill="purple") + 
-  ggtitle("Top 10  least smartest dogs") + labs(x= "Breed", y="Average Number of Repetitions") + theme(text = element_text(size=10),
-                                                                                        axis.text.x = element_text(angle=90, hjust=1))
+ggplot(d2[127:136,],aes(x=reorder(Breed, avg_reps), y=avg_reps)) + geom_bar(stat="identity", fill="purple") + 
+  ggtitle("Top 10 least smartest dogs") + labs(x= "Breed", y="Average Number of Repetitions") + theme(text = element_text(size=10),
+                                                                                         axis.text.x = element_text(angle=90, hjust=1))
 
 
 ggsave("figures/not_smart_dogs.png")
